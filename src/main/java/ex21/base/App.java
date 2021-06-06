cb77529e76035cd47054d6a1db2228ec992415a5
Justin Laptop <cepedaj0601@gmail.com>
2021-06-06 14:52:42 -0400

    Initial solution

--- src/main/java/ex21/base/App.java
+++ src/main/java/ex21/base/App.java
@@ -32,6 +32,53 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("");
+        System.out.println("Please enter the number of the month: ");
+        int number = in.nextInt();
+
+        String month = null;
+
+        switch(number) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                break;
+        }
+
+        System.out.printf("The name of the month is %s", month);
+
     }
 }
